476a5bcf9c30d910d3bfd730b0a4e2c1f3f43e1e
Guilherme <guilherme.prokisch@gmail.com>
2022-03-09 16:58:31 -0300

    refactor: change validation interface

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -12,14 +12,19 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
+    password: '',
     emailError: 'Campo Obrigatório',
     passwordError: 'Campo Obrigatório',
     mainError: ''
   })
 
   useEffect(() => {
-    validation.validate({ email: state.email })
+    validation.validate('email', state.email)
   }, [state.email])
+
+  useEffect(() => {
+    validation.validate('password', state.password)
+  }, [state.password])
 
   return (
     <div className={Styles.login}>

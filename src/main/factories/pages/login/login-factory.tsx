37adccbec140382d7fcd58051de82b6a751f1d59
Guilherme <guilherme.prokisch@gmail.com>
2022-03-13 13:59:15 -0300

    feat: change Login compositon to add SaveAccessToken

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Login from '@/presentation/pages/login/login'
-import { makeRemoteAuthentication } from '@/main/usecases/authentication/remote-authentication-factory'
 import { makeLoginValidation } from './login-validation-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
+import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     < Login
       authentication={makeRemoteAuthentication()}
       validation = {makeLoginValidation()}
+      saveAccessToken ={makeLocalSaveAccessToken()}
     />
   )
 }
